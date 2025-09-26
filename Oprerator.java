38c33952edcf5b199a3e9ce6f4e8b2a29ef58905
praveenm <praveenmpravee7242@gmail.com>
2025-09-26 11:50:59 +0530

    intill comment -added java programing laungage

---
+++ Oprerator.java
@@ -0,0 +1,22 @@
+public class Oprerator {
+    public static void main(String[] args) {
+        int a=10;
+        int b=10;
+         //a=a+5;
+         //a+=2;
+         //a++; // post increment 
+         //++a;//pre increment
+         // by individualy a++ and ++a will give same result but in fetching value it will differ
+
+         int result =a++;
+       //  int result1=b++;
+        //System.out.println("a is : "+a);
+        //System.out.println("b is : "+b);
+        System.out.println("result is a: "+result);
+        //System.out.println("result1 is b: "+result1);
+
+        int x = 5;
+ 
+System.out.println(x++);
+    }
+}
---
+++ TypePromotion.java
@@ -0,0 +1,12 @@
+class TypePromotion{
+    public static void main(String[] args) {
+        byte c=35;
+        byte b = 30;
+       
+        System.out.println("result is: "+b);
+         System.out.println("result is: "+c);
+
+         long result=b*c;
+            System.out.println("result is: "+result);
+    }
+}
