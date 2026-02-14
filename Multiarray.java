03a7da49017e6127b60a573d19af27d727eb7d01
praveenm <praveenmpravee7242@gmail.com>
2026-02-14 08:33:38 +0530

    updated multiarray

--- Multiarray.java
+++ Multiarray.java
@@ -1,12 +1,28 @@
 public class Multiarray{
     public static void main(String args[]){
         int num[][]=new int[3][4];
-        for(int i=0;i<=2;i++)
+       for(int i=0;i<num.length;i++)
         {
-        for(int j=0;j<=3;j++){
+        for(int j=0;j<num[i].length;j++){
+            num[i][j]=(int)(Math.random()*10);
+        }
+        }
+
+
+
+        for(int i=0;i<num.length;i++)
+        {
+        for(int j=0;j<num[i].length;j++){
             System.out.print(num[i][j]+" ");
         }
         System.out.println();
         }
+for(int n[]:num){
+    for(int m:n){
+        System.out.print(m+" ");
+    }
+    System.out.println();
+}
+
     }
 }
