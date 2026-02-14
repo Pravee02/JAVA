696fcab3f264fbb981dc3f29ac9e5d1046a24348
praveenm <praveenmpravee7242@gmail.com>
2026-02-14 06:58:41 +0530

    multi dimentional array

---
+++ Multiarray.java
@@ -0,0 +1,12 @@
+public class Multiarray{
+    public static void main(String args[]){
+        int num[][]=new int[3][4];
+        for(int i=0;i<=2;i++)
+        {
+        for(int j=0;j<=3;j++){
+            System.out.print(num[i][j]+" ");
+        }
+        System.out.println();
+        }
+    }
+}
