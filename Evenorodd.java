f79900f4e440d823c24f89029fd1e96b61655ae9
praveenm <praveenmpravee7242@gmail.com>
2026-02-14 06:14:45 +0530

    evenoradd update

--- Evenorodd.java
+++ Evenorodd.java
@@ -4,6 +4,9 @@ public class Evenorodd{
         int A=30;
         String Result = A%2==0 ? "even" : "odd";
         System.out.println(Result);
+        int b=24;
+        String Result2 = b%2==0 ? "even" : "odd";
+        System.out.println(Result2);
 
     }
 }
