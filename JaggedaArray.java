bcaefe59b78f3bc0b9dc5ec5c51fd50be89f52e4
praveenm <praveenmpravee7242@gmail.com>
2026-02-16 21:20:04 +0530

    jagged

--- JaggedaArray.java
+++ JaggedaArray.java
@@ -1,28 +1,6 @@
-public class Multiarray{
+public class JaggedaArray{
     public static void main(String args[]){
-        int num[][]=new int[3][4];
-       for(int i=0;i<num.length;i++)
-        {
-        for(int j=0;j<num[i].length;j++){
-            num[i][j]=(int)(Math.random()*10);
-        }
-        }
 
-//printing array
-        for(int i=0;i<num.length;i++)
-        {
-        for(int j=0;j<num[i].length;j++){
-            System.out.print(num[i][j]+" ");
-        }
-        System.out.println();
-        }
-// this is new  array printing method 
-        for(int n[]:num){
-            for(int m:n){
-                System.out.print(m+" ");
-            }
-            System.out.println();
-        }
 
 //jagged array 
         int num2[][]=new int[3][];//jagged
