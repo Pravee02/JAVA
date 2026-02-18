0bdeb38d2d55423e84f71f44b2829e010227fa24
praveenm <praveenmpravee7242@gmail.com>
2026-02-18 23:05:41 +0530

    type defination for method overloading

--- Constructor.java
+++ Constructor.java
@@ -26,3 +26,17 @@ class Constructor {
     }
 }
 // also example for overloading 
+
+/* “A constructor is a special method used to initialize objects and is executed automatically when an object is created.”
+
+A constructor is a special method that:
+
+* Has the same name as the class
+* Has no return type
+* Runs automatically when an object is created
+* Is used to initialize (give values to) variables
+* Can be overloaded
+
+Types of Constructor---
+1️. Default Constructor (no parameters)
+2️. Parameterized Constructor (with parameters)
