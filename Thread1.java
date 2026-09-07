34bfacf579fb0a8da3aa9c65d5cfaf3e2023433a
Praveen M <praveenmpravee7242@gmail.com>
2026-09-07 06:07:20 +0530

    updated

--- Thread1.java
+++ Thread1.java
@@ -3,18 +3,27 @@
 {
  public void run()
  {
-    for(int i=0;i<=1000;i++)
+    for(int i=0;i<=100;i++)
     {
         System.out.print("hi  ");
+         try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) { e.printStackTrace();
+        }
     }
  }   
 }
 class b extends Thread{
      public void run()
  {
-    for(int i=0;i<=1000;i++)
+    for(int i=0;i<=100;i++)
     {
         System.out.print("hello  ");
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) { e.printStackTrace();
+        }
+       
     }
  }   
 }
